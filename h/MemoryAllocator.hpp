a4c8363ca6fd586e37800b976f6ec5c8783f3d24
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-10 01:47:45 +0200

    class MemoryAllocator added interface

--- h/MemoryAllocator.hpp
+++ h/MemoryAllocator.hpp
@@ -2,7 +2,36 @@
 // Created by bogdan on 10.7.22..
 //
 
-#ifndef OS_PROJEKAT_MEMORYALLOCATOR_HPP
-#define OS_PROJEKAT_MEMORYALLOCATOR_HPP
 
-#endif //OS_PROJEKAT_MEMORYALLOCATOR_HPP
+
+#ifndef _memoryAllocator_hpp
+#define _memoryAllocator_hpp
+
+#include "../lib/hw.h"
+#include "../lib/console.h"
+class MemoryAllocator{
+
+public:
+
+    struct FreeBlock{
+        FreeBlock* next;
+        FreeBlock* prev;
+        size_t size;
+    };
+
+
+    MemoryAllocator(const MemoryAllocator&) = delete;
+    MemoryAllocator(MemoryAllocator&&) = delete;
+    MemoryAllocator& operator=(const MemoryAllocator&) = delete;
+    MemoryAllocator& operator=(MemoryAllocator&&) = delete;
+
+    static MemoryAllocator& instance();
+    void* memoryMalloc(size_t);
+
+private:
+    FreeBlock *head, *tail;
+    MemoryAllocator();
+
+};
+
+#endif
--- src/MemoryAllocator.cpp
+++ src/MemoryAllocator.cpp
@@ -1,3 +1,23 @@
 //
 // Created by bogdan on 10.7.22..
 //
+
+
+#include "../h/MemoryAllocator.hpp"
+
+
+MemoryAllocator::MemoryAllocator() {
+    head = (FreeBlock*)HEAP_START_ADDR;
+    head->size = (size_t)HEAP_END_ADDR - (size_t)HEAP_START_ADDR - sizeof(FreeBlock);
+    head->next = 0;
+    head->prev = 0;
+    tail = head;
+}
+
+
+MemoryAllocator& MemoryAllocator::instance() {
+    static MemoryAllocator allocatorInstance;
+    return allocatorInstance;
+}
+
+
