8f872546395f96f06b84ce264217aa4ef0c994e4
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-18 17:48:27 +0200

    added print.h

---
+++ h/print.hpp
@@ -0,0 +1,16 @@
+//
+// Created by marko on 20.4.22..
+//
+
+#ifndef OS1_VEZBE07_RISCV_CONTEXT_SWITCH_1_SYNCHRONOUS_PRINT_HPP
+#define OS1_VEZBE07_RISCV_CONTEXT_SWITCH_1_SYNCHRONOUS_PRINT_HPP
+
+#include "../lib/hw.h"
+
+extern void printString(char const *string);
+
+extern void printInteger(uint64 integer);
+
+extern void readString(char []);
+
+#endif //OS1_VEZBE07_RISCV_CONTEXT_SWITCH_1_SYNCHRONOUS_PRINT_HPP
