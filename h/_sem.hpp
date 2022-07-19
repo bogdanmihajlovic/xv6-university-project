a5c29cf23e276644d7794aae22975445538eb070
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-19 21:55:55 +0200

    added class _sem

---
+++ h/_sem.hpp
@@ -0,0 +1,12 @@
+//
+// Created by bogdan on 19.7.22..
+//
+
+#ifndef OS_PROJEKAT__SEM_HPP
+#define OS_PROJEKAT__SEM_HPP
+
+class _sem{
+
+};
+
+#endif //OS_PROJEKAT__SEM_HPP
---
+++ src/_sem.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bogdan on 19.7.22..
+//
+
+#include "../h/_sem.hpp"
