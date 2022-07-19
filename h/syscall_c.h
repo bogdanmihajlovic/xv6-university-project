9a29b36674629be8fd48f949b83633d800ed0df8
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-19 19:33:30 +0200

    added thread_create and thread_dispatch C API

--- h/syscall_c.h
+++ h/syscall_c.h
@@ -2,24 +2,27 @@
 // Created by bogdan on 17.7.22..
 //
 
-#ifndef OS_PROJEKAT_SYSCALL_C_HPP
-#define OS_PROJEKAT_SYSCALL_C_HPP
+#ifndef OS_PROJEKAT_SYSCALL_C_H
+#define OS_PROJEKAT_SYSCALL_C_H
 
 #include "../lib/hw.h"
 
 enum code {
     MEM_ALLOC  = 0x01,
     MEM_FREE = 0x02,
+    THREAD_CREATE = 0x11,
+    THREAD_EXIT = 0x12,
+    THREAD_DISPATCH = 0x13,
     ECALL_USER = 0x08,
     ECALL_SYS = 0x09
 };
 
-extern "C" void* mem_alloc(size_t );
-extern "C" int mem_free(void*);
+void* mem_alloc(size_t );
+int mem_free(void*);
 
-class _thread;
-typedef _thread* thread_t;
-int thread_create ( thread_t* handle, void(*start_routine)(void*), void* arg);
+class TCB;
+typedef TCB* thread_t;
+int thread_create (thread_t* handle, void(*start_routine)(void*), void* arg);
 int thread_exit ();
 void thread_dispatch ();
-#endif //OS_PROJEKAT_SYSCALL_C_HPP
+#endif //OS_PROJEKAT_SYSCALL_C_H
--- src/syscall_c.cpp
+++ src/syscall_c.cpp
@@ -4,13 +4,14 @@
 
 #include "../h/syscall_c.h"
 
-extern "C" void* mem_alloc(size_t size){
+void* mem_alloc(size_t size){
 
     if(size <= 0)
         return 0;
+    size_t temp = size / MEM_BLOCK_SIZE;
+    size_t sz = (temp * MEM_BLOCK_SIZE == size) ? size : (temp + 1)*MEM_BLOCK_SIZE;
 
-
-    __asm__ volatile("mv a1, %[var]" : : [var] "r"(size));
+    __asm__ volatile("mv a1, %[var]" : : [var] "r"(sz));
     __asm__ volatile("mv a0, %[var]" : : [var] "r"(MEM_ALLOC));
     __asm__ volatile("ecall");
     uint64 volatile ret;
@@ -19,7 +20,7 @@ extern "C" void* mem_alloc(size_t size){
     return (void*)ret;
 }
 
-extern "C" int mem_free(void* addr){
+int mem_free(void* addr){
 
     if(!addr)
         return 0;
@@ -32,4 +33,28 @@ extern "C" int mem_free(void* addr){
     __asm__ volatile ("mv %0, a0" : "=r" (ret));
 
     return (int)ret;
+}
+
+int thread_create(thread_t* handle, void(*start_routine)(void*), void* arg){
+
+    void* stack = mem_alloc(sizeof(uint64) * DEFAULT_STACK_SIZE);
+
+    __asm__ volatile("mv a4, %[var]" : : [var] "r"(stack));
+    __asm__ volatile("mv a3, %[var]" : : [var] "r"(arg));
+    __asm__ volatile("mv a2, %[var]" : : [var] "r"(start_routine));
+    __asm__ volatile("mv a1, %[var]" : : [var] "r"(handle));
+    __asm__ volatile("mv a0, %[var]" : : [var] "r"(THREAD_CREATE));
+    __asm__ volatile("ecall");
+
+    uint64 volatile ret;
+    __asm__ volatile ("mv %0, a0" : "=r" (ret));
+    return (int)ret;
+
+}
+int thread_exit (){
+    return 1;
+}
+void thread_dispatch (){
+    __asm__ volatile("mv a0, %[var]" : : [var] "r"(THREAD_DISPATCH));
+    __asm__ volatile("ecall");
 }
