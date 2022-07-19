07508ae811488e86717200b1271502561cc3e936
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-19 16:14:46 +0200

    added interface for C++ API - syscall_cpp.h

---
+++ h/syscall_cpp.h
@@ -0,0 +1,43 @@
+//
+// Created by bogdan on 19.7.22..
+//
+
+#ifndef OS_PROJEKAT_SYSCALL_CPP_H
+#define OS_PROJEKAT_SYSCALL_CPP_H
+
+void* ::operator new (size_t);
+void ::operator delete (void*);
+class Thread {
+        public:
+        Thread (void (*body)(void*), void* arg);
+        virtual ~Thread ();
+        int start ();
+        static void dispatch ();
+        static int sleep (time_t);
+        protected:
+        Thread ();
+        virtual void run () {}
+        private:
+        thread_t myHandle;
+};
+class Semaphore {
+        public:
+        Semaphore (unsigned init = 1);
+        virtual ~Semaphore ();
+        int wait ();
+        int signal ();
+        private:
+        sem_t myHandle;
+};
+class PeriodicThread : public Thread {
+        protected:
+        PeriodicThread (time_t period);
+        virtual void periodicActivation () {}
+};
+class Console {
+        public:
+        static char getc ();
+        static void putc (char);
+};
+
+#endif //OS_PROJEKAT_SYSCALL_CPP_H
