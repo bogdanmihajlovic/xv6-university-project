0f6a3c97ccc5ae71f632b62be0bd9f065db2c9a7
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-19 23:09:24 +0200

    implemented class _sem

--- h/_sem.hpp
+++ h/_sem.hpp
@@ -4,9 +4,24 @@
 
 #ifndef OS_PROJEKAT__SEM_HPP
 #define OS_PROJEKAT__SEM_HPP
+#include "../h/syscall_c.h"
+#include "../h/list.hpp"
+#include "../h/TCB.hpp"
 
 class _sem{
+public:
+    static int createSemaphore(sem_t* handle, unsigned value);
 
+    void wait();
+    void signal();
+protected:
+    void block();
+    void unblock();
+private:
+    _sem(unsigned value) : value(value){}
+
+    unsigned value;
+    List<TCB> blocked;
 };
 
 #endif //OS_PROJEKAT__SEM_HPP
--- src/_sem.cpp
+++ src/_sem.cpp
@@ -3,3 +3,34 @@
 //
 
 #include "../h/_sem.hpp"
+
+int _sem::createSemaphore(sem_t *handle, unsigned int i) {
+    *handle = new _sem(i);
+    return 0;
+}
+
+void _sem::wait() {
+    // TODO lock()
+    if(--value < 0)
+        block();
+    // TODO unlock()
+}
+
+void _sem::signal() {
+    // TODO lock()
+    if(++value <= 0)
+        unblock();
+    // TODO unlock()
+}
+
+void _sem::block() {
+    blocked.addLast(TCB::running);
+    // TODO sredi TCB klasu
+}
+
+void _sem::unblock() {
+    TCB* t = blocked.removeFirst();
+    Scheduler::put(t);
+}
+
+
