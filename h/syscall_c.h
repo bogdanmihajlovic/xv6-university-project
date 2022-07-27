df7108e7911793355df17dce6fff3c3b8bf50195
bogdanmihajlovic <bogdanmihajlovic4@gmail.com>
2022-07-27 22:56:32 +0200

    C API finished

--- h/syscall_c.h
+++ h/syscall_c.h
@@ -37,6 +37,7 @@ int thread_create (thread_t* handle, void(*start_routine)(void*), void* arg);
 int thread_exit ();
 void thread_dispatch ();
 
+
 class _sem;
 typedef _sem* sem_t;
 int sem_open(sem_t* handle, unsigned init);
